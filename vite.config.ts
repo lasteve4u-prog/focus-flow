25e9d65a99f672356db12acd069fc1821164699c
Yuki <lasteve4u@gmail.com>
2026-01-11 18:10:39 +0900

    Update: App icon to Zundamon theme (Favicon, Apple Touch, PWA)

--- vite.config.ts
+++ vite.config.ts
@@ -21,17 +21,17 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'zundamon-icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'zundamon-icon.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'zundamon-icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
